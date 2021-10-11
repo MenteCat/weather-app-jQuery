6dee87c18874cc77ac8b2c621727378d4fa1cb9d
Tina Iuso <tinaiuso@gmail.com>
2021-10-11 18:41:09 +0100

    READme.md updated with a walkthrough the app

---
+++ index.js
@@ -0,0 +1,22 @@
+/* We start selecting the entire document and then we call the .ready 
+method on it and then inside the method is where all 
+the code goes */
+
+$(document).ready(() => {
+  const searchButton = $(".search-button")
+  const userInput = $(".user-input")
+  /* .hide() method when the page is loaded 
+  as we don't want it to be shown */ 
+  const weatherData = $(".weather-data").hide()
+  const error = $(".error").hide()
+
+
+
+
+
+
+
+
+
+
+})
